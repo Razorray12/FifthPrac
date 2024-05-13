80fdd6c15bbc38320dc560aad9378f76ad9d68eb
Razorray <den.potemkin2015@mail.ru>
2024-05-13 11:36:58 +0300

    1

---
+++ app/src/main/java/com/example/fifthprac/ScrollActivity.java
@@ -0,0 +1,13 @@
+package com.example.fifthprac;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScrollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll);
+    }
+}
